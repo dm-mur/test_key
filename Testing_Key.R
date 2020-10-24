095a45f348802b3bb0ca4792e1fe172bb183f07d
Muriungi <DMuriungi@umsomihvkenya.local>
2020-10-24 13:25:28 +0300

    My first connection check

---
+++ Testing_Key.R
@@ -0,0 +1,6 @@
+source('~/.active-rstudio-document', echo=TRUE)
+source('~/.active-rstudio-document', echo=TRUE)
+source('~/.active-rstudio-document', echo=TRUE)
+print("This file was created within RStudio")
+print("And now it lives on GitHub")
+
